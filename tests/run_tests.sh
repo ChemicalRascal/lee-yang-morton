8a3a5c425214770e22f1a4971af3f66b3482f09c
James Denholm <nod.helm@gmail.com>
2017-04-22 08:04:21 +1000

    Hide testing data filespam in tests/data

--- tests/run_tests.sh
+++ tests/run_tests.sh
@@ -5,9 +5,10 @@
 #               randomly-generated tree of size $1 and a point probability
 #               of $2, and compare the python output with the "real" output.
 
-PREFIX=$(date "+%Y.%m.%d.%T")
+PREFIX=data/$(date "+%Y.%m.%d.%T")
 MAX=$(python -c "print(2**$1 - 1)")
 
+mkdir data
 touch $PREFIX.csv
 ./gen_tree_input.py $1 $2 > $PREFIX.csv &&
 echo "csv made" &&
