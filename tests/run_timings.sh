6a418bc45666ce13d2cb58e03b27150328a56b9a
James Denholm <nod.helm@gmail.com>
2017-09-15 01:20:01 +1000

    Add first-pass run_timings.sh

---
+++ tests/run_timings.sh
@@ -0,0 +1,21 @@
+#!/bin/env sh
+# vim: set et sts=4 sw=4 cc=80 tw=80: 
+#
+# run_timings.sh: Runs timing code against $PREFIX.queries_$SUFFIX -es.
+# 
+#                 $1:  $PREFIX
+#                 $2+: List of "modes" ("-c n" as "-cn")
+
+PREFIX=$1
+
+for i in `ls -1 $PREFIX.queries_*`; do
+    echo "$i"
+    SUFFIX=${i#$PREFIX.queries_}
+    echo "$SUFFIX" > $PREFIX.res
+    for j in ${@#$1}; do
+        echo $j
+        ./../qtree_testbench -qtx "$PREFIX" $j < $i > $PREFIX.res
+    done
+    echo "----" > $PREFIX.res
+    true
+done
