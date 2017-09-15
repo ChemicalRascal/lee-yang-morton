a1358ca4726d2213be4cf113697e22c7baa8ae75
James Denholm <nod.helm@gmail.com>
2017-09-15 10:21:03 +1000

    First pass on prep_Binaries.sh script
    Things are verrrrry slow on my local machine, time to see how vincent
    handles it.

---
+++ data_sets/prep_Binaries.sh
@@ -0,0 +1,9 @@
+#!/bin/env sh
+# vim: set et sts=4 sw=4 cc=80 tw=80: 
+#
+# prep_Binaries.sh
+#
+#   $1: $PREFIX
+
+#./../qtree_testbench -x $1 -b -c 16 -c 32 -c 512 -c 2000 -c 20000 -f -g -q
+./../qtree_testbench -x $1 -b -c 16
