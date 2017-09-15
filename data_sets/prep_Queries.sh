a2883fe29197b255e2fe9678491837f4bd474912
James Denholm <nod.helm@gmail.com>
2017-09-15 10:07:23 +1000

    Add data_sets/prep_Queries.sh script

---
+++ data_sets/prep_Queries.sh
@@ -0,0 +1,12 @@
+#!/bin/env sh
+# vim: set et sts=4 sw=4 cc=80 tw=80: 
+#
+# prep_Queries.sh
+#
+#   $1: $PREFIX
+#   $2: Number of sets to generate
+#   $3: Number of queries per set
+
+for i in `seq 1 $3`; do
+    ./../gen_queries_percent -x $1 -f $1.queries_$i -g -3 -h 3 -i 0.01 -n $3
+done
