15c9c901ae098bf7bcf77c06e7dfe9a220e43098
James Denholm <nod.helm@gmail.com>
2017-09-21 05:28:58 +1000

    Make prep_Queries vary on size, not |Q|

--- data_sets/prep_Queries.sh
+++ data_sets/prep_Queries.sh
@@ -1,16 +1,18 @@
 #!/usr/bin/env sh
-# vim: set et sts=4 sw=4 cc=80 tw=80: 
+# vim: set et sts=4 sw=4: 
 #
 # prep_Queries.sh
 #
 #   $1:  $PREFIX
 #   $2:  Number of sets to generate
-#   $3+: Number of queries per set (avoid duplicates!)
+#   $3:  Number of queries per set
+#   $4+: Size of each query (avoid duplicates)
 
 v=${@#$1}
 v=${v#$2}
+v=${v#$3}
 for i in $v; do
     for j in `seq 1 $2`; do
-        ./../gen_queries_percent -x $1 -f ${1}.queries_${i}_${j} -g -3 -h 3 -i 0.01 -n $i
+        ./../gen_queries_percent -x $1 -f ${1}.queries_${i}_${j} -g -3 -h 3 -i $i -n $3
     done
 done
