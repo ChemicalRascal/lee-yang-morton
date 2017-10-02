ffed61a83a2d93554a084f257424e9731f37688d
James Denholm <nod.helm@gmail.com>
2017-10-03 04:12:49 +1100

    Add validation mode skeleton

--- src/qtree_testbench.cpp
+++ src/qtree_testbench.cpp
@@ -232,6 +232,7 @@ exit_fprintf_help(char** argv)
     fprintf(stdout, "  -p           print mode\n");
     fprintf(stdout, "  -b           build mode (otherwise, search mode)\n");
     fprintf(stdout, "  -t           timing mode\n");
+    fprintf(stdout, "  -v           validation mode\n");
     fprintf(stdout, "\n");
     fprintf(stdout, "  -c Q         QSI w/ psum quantum of Q:   .qsi_$Q\n");
     fprintf(stdout, "  -d           Binary Quad Tree:           .bqt\n");
@@ -253,7 +254,7 @@ exit_fprintf_usage(char** argv)
 int
 main(int argc, char** argv, char** envp)
 {
-    int opt, build_mode, print_mode, timing_mode;
+    int opt, build_mode, print_mode, timing_mode, validation_mode;
     FILE* input_fp;
     unsigned int maxatt;
 
@@ -295,6 +296,7 @@ main(int argc, char** argv, char** envp)
     build_mode = 0;
     print_mode = 0;
     timing_mode = 0;
+    validation_mode = 0;
     global_prefix_arg = NULL;
     input_fp = NULL;
 
@@ -316,6 +318,9 @@ main(int argc, char** argv, char** envp)
                 break;
             case 't':
                 timing_mode = 1;
+                break;
+            case 'v':
+                validation_mode = 1;
                 break;
             case 'c':
                 mode_l.push_back(std::tuple<opmode_t, unsigned int>
@@ -511,168 +516,181 @@ main(int argc, char** argv, char** envp)
         std::tuple<opmode_t, unsigned int> mode_flag =
             std::tuple<opmode_t, unsigned int>(null_mode, 0);
 
-        // Work out which mode we should be in -- Multiple can be specified,
-        // but only the final one matters.
         if (mode_l.empty())
         {
             exit_fprintf_usage(argv);
         }
-        mode_flag = mode_l.back();
-        if (std::get<0>(mode_flag) == null_mode)
-        {
-            exit_fprintf_usage(argv);
-        }
 
-        readcsv_get_uint(fopen((prefix + ".maxatt").c_str(), "r"), &maxatt);
-        switch (std::get<0>(mode_flag))
+        // Code duplication is fun
+        if (validation_mode != 1)
         {
-            case qsi_mode:
-                tree_file = std::fstream((prefix + ".qsi_" +
-                            std::to_string(std::get<1>(mode_l.front()))
-                            ).c_str(),
-                        std::fstream::binary | std::fstream::in);
-                qsiseq = read_qsiseq(tree_file);
-                break;
-            case bqt_mode:
-                tree_file = std::fstream((prefix + ".bqt").c_str(),
-                        std::fstream::binary | std::fstream::in);
-                bitqtree.load(tree_file);
-                break;
-            case oqt_mode:
-                tree_file = std::fstream((prefix + ".oqt").c_str(),
-                        std::fstream::binary | std::fstream::in);
-                oqt.load(tree_file);
-                break;
-            case sdsl_k2_mode:
-                tree_file = std::fstream((prefix + ".k2").c_str(),
-                        std::fstream::binary | std::fstream::in);
-                k2.load(tree_file);
-                break;
-            case ofb_mode:
-                tree_file = std::fstream((prefix + ".ofb").c_str(),
-                        std::fstream::binary | std::fstream::in);
-                ofb.load(tree_file);
-                break;
-            default:
+            // Work out which mode we should be in -- Multiple can be specified,
+            // but only the final one matters.
+            if (mode_l.empty())
+            {
                 exit_fprintf_usage(argv);
-                break;
-        }
+            }
+            mode_flag = mode_l.back();
+            if (std::get<0>(mode_flag) == null_mode)
+            {
+                exit_fprintf_usage(argv);
+            }
 
-        if (print_mode == 1)
-        {
+            readcsv_get_uint(fopen((prefix + ".maxatt").c_str(), "r"), &maxatt);
             switch (std::get<0>(mode_flag))
             {
                 case qsi_mode:
-                    pprint_qsiseq(qsiseq);
+                    tree_file = std::fstream((prefix + ".qsi_" +
+                                std::to_string(std::get<1>(mode_l.front()))
+                                ).c_str(),
+                            std::fstream::binary | std::fstream::in);
+                    qsiseq = read_qsiseq(tree_file);
                     break;
                 case bqt_mode:
-                    printf("bqt pprint() not implemented.\n");
+                    tree_file = std::fstream((prefix + ".bqt").c_str(),
+                            std::fstream::binary | std::fstream::in);
+                    bitqtree.load(tree_file);
                     break;
                 case oqt_mode:
-                    oqt.pprint();
+                    tree_file = std::fstream((prefix + ".oqt").c_str(),
+                            std::fstream::binary | std::fstream::in);
+                    oqt.load(tree_file);
                     break;
                 case sdsl_k2_mode:
-                    printf("k2 pprint() not implemented.\n");
+                    tree_file = std::fstream((prefix + ".k2").c_str(),
+                            std::fstream::binary | std::fstream::in);
+                    k2.load(tree_file);
                     break;
                 case ofb_mode:
-                    ofb.pprint();
+                    tree_file = std::fstream((prefix + ".ofb").c_str(),
+                            std::fstream::binary | std::fstream::in);
+                    ofb.load(tree_file);
                     break;
                 default:
                     exit_fprintf_usage(argv);
                     break;
             }
-        }
 
-        std::vector<std::tuple<vec_size_type, vec_size_type, vec_size_type,
-            vec_size_type, vec_size_type>> query_vec =
-                read_range_queries_to_vector(stdin);
-        std::vector<std::tuple<vec_size_type, vec_size_type, vec_size_type,
-            vec_size_type, vec_size_type>>::iterator qvi;
+            if (print_mode == 1)
+            {
+                switch (std::get<0>(mode_flag))
+                {
+                    case qsi_mode:
+                        pprint_qsiseq(qsiseq);
+                        break;
+                    case bqt_mode:
+                        printf("bqt pprint() not implemented.\n");
+                        break;
+                    case oqt_mode:
+                        oqt.pprint();
+                        break;
+                    case sdsl_k2_mode:
+                        printf("k2 pprint() not implemented.\n");
+                        break;
+                    case ofb_mode:
+                        ofb.pprint();
+                        break;
+                    default:
+                        exit_fprintf_usage(argv);
+                        break;
+                }
+            }
 
-        switch (std::get<0>(mode_flag))
-        {
-            case qsi_mode:
-                gettimeofday(&t_00, NULL);
-                for (qvi = query_vec.begin(); qvi != query_vec.end(); qvi++)
-                {
-                    std::get<4>(*qvi) = fast_lee_yang_qsi(qsiseq,
-                            std::get<0>(*qvi), std::get<1>(*qvi),
-                            std::get<2>(*qvi), std::get<3>(*qvi));
-                }
-                gettimeofday(&t_01, NULL);
-                break;
-            case bqt_mode:
-                gettimeofday(&t_00, NULL);
-                printf("bqt querying not implemented.\n");
-                gettimeofday(&t_01, NULL);
-                break;
-            case oqt_mode:
-                //FIXME: This
-                gettimeofday(&t_00, NULL);
-                printf("oqt querying not implemented.\n");
-                gettimeofday(&t_01, NULL);
-                break;
-            case sdsl_k2_mode:
-                gettimeofday(&t_00, NULL);
-                for (qvi = query_vec.begin(); qvi != query_vec.end(); qvi++)
-                {
-                    std::get<4>(*qvi) = k2.range_count(
-                            std::get<0>(*qvi), std::get<2>(*qvi),
-                            std::get<1>(*qvi), std::get<3>(*qvi));
-                }
-                gettimeofday(&t_01, NULL);
-                break;
-            case ofb_mode:
-                gettimeofday(&t_00, NULL);
-                for (qvi = query_vec.begin(); qvi != query_vec.end(); qvi++)
-                {
-                    std::get<4>(*qvi) = ofb.range_count(
-                            std::get<0>(*qvi), std::get<2>(*qvi),
-                            std::get<1>(*qvi), std::get<3>(*qvi));
-                }
-                gettimeofday(&t_01, NULL);
-                break;
-            default:
-                gettimeofday(&t_00, NULL);
-                exit_fprintf_usage(argv);
-                gettimeofday(&t_01, NULL);
-                break;
-        }
-        batch_sec = t_01.tv_sec - t_00.tv_sec;
-        batch_usec = t_01.tv_usec - t_00.tv_usec;
-        if (batch_usec < 0)
-        {
-            batch_usec += 1000000;
-            batch_sec -= 1;
-        }
+            std::vector<std::tuple<vec_size_type, vec_size_type, vec_size_type,
+                vec_size_type, vec_size_type>> query_vec =
+                    read_range_queries_to_vector(stdin);
+            std::vector<std::tuple<vec_size_type, vec_size_type, vec_size_type,
+                vec_size_type, vec_size_type>>::iterator qvi;
 
-        if (timing_mode == 1)
-        {
             switch (std::get<0>(mode_flag))
             {
                 case qsi_mode:
-                    printf("qsi_%06d ", std::get<1>(mode_flag));
+                    gettimeofday(&t_00, NULL);
+                    for (qvi = query_vec.begin(); qvi != query_vec.end(); qvi++)
+                    {
+                        std::get<4>(*qvi) = fast_lee_yang_qsi(qsiseq,
+                                std::get<0>(*qvi), std::get<1>(*qvi),
+                                std::get<2>(*qvi), std::get<3>(*qvi));
+                    }
+                    gettimeofday(&t_01, NULL);
+                    break;
+                case bqt_mode:
+                    gettimeofday(&t_00, NULL);
+                    printf("bqt querying not implemented.\n");
+                    gettimeofday(&t_01, NULL);
+                    break;
+                case oqt_mode:
+                    //FIXME: This
+                    gettimeofday(&t_00, NULL);
+                    printf("oqt querying not implemented.\n");
+                    gettimeofday(&t_01, NULL);
                     break;
                 case sdsl_k2_mode:
-                    printf("sdsl_k2    ");
+                    gettimeofday(&t_00, NULL);
+                    for (qvi = query_vec.begin(); qvi != query_vec.end(); qvi++)
+                    {
+                        std::get<4>(*qvi) = k2.range_count(
+                                std::get<0>(*qvi), std::get<2>(*qvi),
+                                std::get<1>(*qvi), std::get<3>(*qvi));
+                    }
+                    gettimeofday(&t_01, NULL);
                     break;
                 case ofb_mode:
-                    printf("ofb        ");
+                    gettimeofday(&t_00, NULL);
+                    for (qvi = query_vec.begin(); qvi != query_vec.end(); qvi++)
+                    {
+                        std::get<4>(*qvi) = ofb.range_count(
+                                std::get<0>(*qvi), std::get<2>(*qvi),
+                                std::get<1>(*qvi), std::get<3>(*qvi));
+                    }
+                    gettimeofday(&t_01, NULL);
                     break;
                 default:
-                    printf("????????   ");
+                    gettimeofday(&t_00, NULL);
+                    exit_fprintf_usage(argv);
+                    gettimeofday(&t_01, NULL);
                     break;
             }
-            printf("%04ld.%06ld sec.usec\n", batch_sec, batch_usec);
-        }
-        else
-        {
-            for (qvi = query_vec.begin(); qvi != query_vec.end(); qvi++)
+            batch_sec = t_01.tv_sec - t_00.tv_sec;
+            batch_usec = t_01.tv_usec - t_00.tv_usec;
+            if (batch_usec < 0)
             {
-                printf("%lu,%lu %lu,%lu: %lu\n", std::get<0>(*qvi),
-                        std::get<1>(*qvi), std::get<2>(*qvi), std::get<3>(*qvi),
-                        std::get<4>(*qvi));
+                batch_usec += 1000000;
+                batch_sec -= 1;
             }
+
+            if (timing_mode == 1)
+            {
+                switch (std::get<0>(mode_flag))
+                {
+                    case qsi_mode:
+                        printf("qsi_%06d ", std::get<1>(mode_flag));
+                        break;
+                    case sdsl_k2_mode:
+                        printf("sdsl_k2    ");
+                        break;
+                    case ofb_mode:
+                        printf("ofb        ");
+                        break;
+                    default:
+                        printf("????????   ");
+                        break;
+                }
+                printf("%04ld.%06ld sec.usec\n", batch_sec, batch_usec);
+            }
+            else
+            {
+                for (qvi = query_vec.begin(); qvi != query_vec.end(); qvi++)
+                {
+                    printf("%lu,%lu %lu,%lu: %lu\n", std::get<0>(*qvi),
+                            std::get<1>(*qvi), std::get<2>(*qvi), std::get<3>(*qvi),
+                            std::get<4>(*qvi));
+                }
+            }
+        } // if (validation_mode != 1)
+        else if (validation_mode == 1)
+        {
+            // TODO: Validation mode
         }
     }
 
