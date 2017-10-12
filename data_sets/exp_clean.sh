ea51e8a40456bbdad7a3127551c40730595ce541
James Denholm <nod.helm@gmail.com>
2017-10-13 08:07:14 +1100

    Add results

---
+++ data_sets/exp_clean.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env zsh
+# vim: set et sts=4 sw=4 cc=80:
+#
+# exp_clean.sh: Just do everything, for a given definition of everything.
+
+eval ./clear_results.sh\ allCountries{.5kr1,.5kr2,.5kr3,.5kr4,.5kr5,.40kr1,.40kr2,.50kr1,.50kr2,.60kr1,.60kr2,.500kr,.1mr,.2mr,}\;
