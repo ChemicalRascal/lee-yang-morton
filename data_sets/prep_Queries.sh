a42d8ef99eaf388c68d648d6443daa2a7e163fa1
James Denholm <nod.helm@gmail.com>
2018-01-02 09:33:20 +1100

    Cleanup /data_sets/ stuff

--- data_sets/prep_Queries.sh
+++ data_sets/prep_Queries.sh
@@ -9,8 +9,11 @@
 #   $4+: Size of each query (avoid duplicates)
 
 v=${@#$1}
-v=${v# $2}
-v=${v# $3}
+v=${v#" "}
+v=${v#$2}
+v=${v#" "}
+v=${v#$3}
+v=${v#" "}
 for i in $v; do
     for j in `seq 1 $2`; do
         ./../gen_queries_percent -x $1 -f ${1}.queries_${i}_${j} -i $i -n $3
